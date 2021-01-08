b0b0021d15bf2e99b1c77ccaa2edd74d033e5450
cordenbrock <cordenbrock@gmail.com>
2021-01-08 09:08:06 -0800

    Add directives and namespaces

---
+++ Factory/Controllers/EngineersController.cs
@@ -0,0 +1,10 @@
+using Factory.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Factory.Controllers
+{
+  
+}
